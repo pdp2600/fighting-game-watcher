28f41a5d033c4edaa734a576b2d55ef37ff44146
PDP2600 <jacks_smirking_revenge@hotmail.com>
2019-07-16 15:47:12 -0400

    Initial commit

---
+++ initial_experiments.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 26 14:04:42 2019
+
+@author: PDP2600
+"""
+from imutils.object_detection import non_max_suppression
+import numpy as np
+import cv2
+#import tesserocr
+import pytesseract
+
